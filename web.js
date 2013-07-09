321b4dc467aeaaa434b718df5dc0f32731875225
UBio <ubio.bioinfo@gmail.com>
2013-07-09 15:27:23 +0000

    index.html

--- web.js
+++ web.js
@@ -2,17 +2,14 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-//app.get('/', function(request, response) {
-//  response.send('Hello World 3!');
+var buffer = new Buffer("I'm a string!", "utf-8")
+//fs.readFile('index.html', function (err, data) {
+//  if (err) throw err;
+//	app.get('/', function(request, response) {
+//		response.send(data);
+//	});
+ // console.log(data);
 //});
-
-fs.readFile('index.html', function (err, data) {
-  if (err) throw err;
-	app.get('/', function(request, response) {
-		response.send(data);
-	});
-  console.log(data);
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
