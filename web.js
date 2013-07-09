0b732703ce921d376427d4e3bff8541b62bf0d07
UBio <ubio.bioinfo@gmail.com>
2013-07-09 15:21:41 +0000

    index.html

--- web.js
+++ web.js
@@ -8,6 +8,9 @@ var app = express.createServer(express.logger());
 
 fs.readFile('index.html', function (err, data) {
   if (err) throw err;
+	app.get('/', function(request, response) {
+		response.send(data);
+	});
   console.log(data);
 });
 
